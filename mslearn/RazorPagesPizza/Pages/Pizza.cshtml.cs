822dc67998829884a717479921f5a59be09243f1
Vladimir Mikhailov <vladimir@mikhailov.in>
2022-10-03 23:07:33 +0300

    mslearn: Razor Pages

---
+++ mslearn/RazorPagesPizza/Pages/Pizza.cshtml.cs
@@ -0,0 +1,43 @@
+using RazorPagesPizza.Models;
+using RazorPagesPizza.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesPizza.Pages
+{
+  public class PizzaModel : PageModel
+  {
+    public List<Pizza> pizzas = new();
+
+    [BindProperty]
+    public Pizza NewPizza { get; set; } = new();
+
+    public void OnGet()
+    {
+      pizzas = PizzaService.GetAll();
+    }
+
+    public string GlutenFreeText(Pizza pizza)
+    {
+      return pizza.IsGlutenFree ? "Gluten Free" : "Not Gluten Free";
+    }
+
+    public IActionResult OnPostAsync()
+    {
+      if (!ModelState.IsValid)
+      {
+        return Page();
+      }
+      PizzaService.Add(NewPizza);
+      return RedirectToAction("Get");
+    }
+
+    public IActionResult OnPostDelete(int id)
+    {
+      PizzaService.Delete(id);
+      return RedirectToAction("Get");
+    }
+  }
+
+
+}
